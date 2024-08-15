750591d5e9e808c413ff7602128cefa37fe0b22e
guicvsub <rm552321@fiap.com.br>
2024-08-15 17:42:02 -0300

    Exercicios

---
+++ Exercicio04.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+public class Exercicio04 {
+    public static void main(String args[] ){
+
+        Scanner scanner = new Scanner(System.in);
+        int numero = scanner.nextInt();
+        int sucessor= numero+1;
+        int atecessor= numero-1;
+        System.out.println("o sucessor do "+sucessor );
+        System.out.println("o aticessor e "+atecessor);
+
+    }
+}
