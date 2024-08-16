2757d24e7061ab1b18ee5ef3d8f3ae629742cab8
guicvsub <rm552321@fiap.com.br>
2024-08-16 15:55:52 -0300

    iniciandoBateria3

---
+++ Bateria2Exercicio03.java
@@ -0,0 +1,12 @@
+public class Bateria2Exercicio03 {
+    public static void main(String args[]){
+        double r =3.0;
+        double a = 5.5;
+        double formula = 3.14159*(r*r)*a;
+        double result = Math.round(formula);
+        System.out.println("o volume dq lata de olho e " + result+"formato da lata considerada foi cilindrico");
+
+    }
+}
+
+
