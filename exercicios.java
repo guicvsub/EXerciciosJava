ef20919da3661df0b7f4835e7e590352cbc806ba
guicvsub <143191040+guicvsub@users.noreply.github.com>
2024-09-24 22:33:15 -0300

    Create exercicios.java
    exercicios java

---
+++ exercicios.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //StringBuilder fraser = new StringBuilder("hello");
+        //fraser.insert(5, " world");
+       // System.out.println(fraser);
+
+//String frase = "o pai e foda patroa";
+//String frase2 = frase.replace(" ","");
+        //System.out.println(frase2.length());
+
+//String text= "vai corinthians";
+//String text2 = text.substring(0,3);
+//System.out.println(text2);
+
+//String texto ="corinthians 2x0 hoje";
+//String texto2 = "corinthuans campeao da copa do brasil";
+    ///    System.out.println(texto.equals(texto2));
+       // String text3 = texto2;
+        //System.out.println(text3.equals(texto2));
+
+
+//Scanner sc = new Scanner(System.in);
+//StringBuffer sb = new StringBuffer(sc.nextLine());
+  //      System.out.println( sb.reverse());
+
+        //String frase = "dinheiro compra amor verdadeiro?";
+        //String Frase2= frase.replace("a","o");
+        //System.out.println(Frase2);
+
+//String frase = " essa frase nao contem espacos ";
+//String semespacos= frase.trim();
+//String frasereplace = semespacos.replace(" ","");
+//System.out.println(frasereplace);
+
+ //String frase = "essa e a frase teste";
+ //       int contador = 0;
+ //for (int i = 0; i < frase.length(); i++) {
+   //  if (frase.charAt(i) == 'e') {
+     //   contador++;
+       //  System.out.println(contador);
+   // }
+ //}
+
+
+        //String frase = "se leu e viado";
+       // String[] newFrase = frase.split(" ");
+        //for (String s : newFrase) {
+          //  System.out.println(s);
+        //};
+
+        //String texto ="calma";
+
+
+        //String texto1 =" cara " ;
+        //String texto2 =" foi " ;
+        //String texto3 =" uma " ;
+        //String texto4 =" piadinha " ;
+        //StringBuilder frase = new StringBuilder(texto+texto1+texto2+texto3+texto4);
+        //System.out.println(frase.toString());
+
+    }
+
+}
